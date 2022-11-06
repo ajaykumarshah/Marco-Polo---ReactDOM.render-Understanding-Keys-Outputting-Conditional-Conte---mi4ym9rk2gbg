c8e982ac1da49925c896953fb73f3a3706b87dbf
ajay kumar  shah <110763605+ajaykumarshah@users.noreply.github.com>
2022-11-07 00:00:49 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { useState } from 'react';
 import '../styles/App.css';
 const App = () => {
-  
+
+  const [innerelement, setInnerelement] = useState("Marco");
+  const [innerelement2, setInnerelement2] = useState("Polo");
+
+  function setter() {
+    let temp = innerelement;
+    setInnerelement(innerelement2);
+    setInnerelement2(temp);
+  }
+
   return (
     <div id="main">
-
+      <h1 id='marco-polo'>{innerelement}</h1>
+      <button id='marco-polo-toggler' onClick={setter} >{innerelement2}</button>
     </div>
   )
 }
